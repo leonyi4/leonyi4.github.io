a48ae6ac6db4ceb621baa1f21210c19bbf3718f9
Leo <114857318+leonyi4@users.noreply.github.com>
2024-02-03 22:08:56 +0100

    set up routes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,18 +1,113 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 const Nav = () => {
   return (
     <>
-      <nav>
-        <div></div>
-        <div>
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Projects</li>
-            <li>Contact</li>
-            <li>Resume</li>
-          </ul>
+      <nav className="w-full flex p-4 justify-between items-center mx-auto max-w-screen-xl">
+        <div className="">Leo</div>
+        <ul className="flex ">
+          <li>
+            <Link to={"home"}>Home</Link>
+          </li>
+          <li>
+            <Link to={"about"}>About</Link>
+          </li>
+          <li>
+            <Link to={"projects"}>Projects</Link>
+          </li>
+          <li>
+            <Link to={"contact"}>Contact</Link>
+          </li>
+          <li>
+            <button>Resume</button>
+          </li>
+        </ul>
+      </nav>
+      <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
+        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+          <Link to="/" className="flex items-center">
+            <img
+              src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
+              className="mr-3 h-12"
+              alt="Logo"
+            />
+          </Link>
+          <div className="flex items-center lg:order-2">
+            <Link
+              to="#"
+              className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+            >
+              Resume
+            </Link>
+          </div>
+          <div
+            className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
+            id="mobile-menu-2"
+          >
+            <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+              <li>
+                <NavLink
+                  to="/"
+                  className={({ isActive }) =>
+                    `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 
+                                        ${
+                                          isActive
+                                            ? "text-orange-700"
+                                            : "text-gray-700"
+                                        } lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                  }
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/about"
+                  className={({ isActive }) =>
+                    `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 
+                                        ${
+                                          isActive
+                                            ? "text-orange-700"
+                                            : "text-gray-700"
+                                        } lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                  }
+                >
+                  About
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/projects"
+                  className={({ isActive }) =>
+                    `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 
+                                        ${
+                                          isActive
+                                            ? "text-orange-700"
+                                            : "text-gray-700"
+                                        } lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                  }
+                >
+                  Projects
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/contact"
+                  className={({ isActive }) =>
+                    `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 
+                                        ${
+                                          isActive
+                                            ? "text-orange-700"
+                                            : "text-gray-700"
+                                        } lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                  }
+                >
+                  Contact
+                </NavLink>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     </>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import Contact from "./Contact/Contact.jsx";
+import Layout from "./Layout.jsx";
+import About from "./components/About/About.jsx";
+import Home from "./components/Home/Home.jsx";
+import Projects from "./components/Projects/Projects.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="home" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="projects" element={<Projects />} />
+      <Route path="contact" element={<Contact />} />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
