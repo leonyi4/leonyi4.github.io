f29bf5293cc4d0b3b23d99e7a37fee73cd0cf25a
Leo <114857318+leonyi4@users.noreply.github.com>
2024-02-03 21:25:03 +0100

    installed tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <>
-      <h1>Hello World!</h1>
+      <Nav></Nav>
+      <h1 className="text-3xl font-bold underline">Hello World!</h1>
     </>
   );
 }
---
+++ src/components/Nav.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <>
+      <nav>
+        <div></div>
+        <div>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Projects</li>
+            <li>Contact</li>
+            <li>Resume</li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Nav;
